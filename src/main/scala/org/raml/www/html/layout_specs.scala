733f4c86f2fc29b859450f22300ceba76be831af
aldonline <aldo.bucchi@gmail.com>
2015-01-28 17:08:44 -0300

    change spec title

--- src/main/scala/org/raml/www/html/layout_specs.scala
+++ src/main/scala/org/raml/www/html/layout_specs.scala
@@ -8,7 +8,7 @@ object layout_specs extends HtmlBase {
       <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta charset="utf-8"/>
-        <title>RAML 0.8</title>
+        <title>RAML 0.8 Specification</title>
         { css("stylesheets/github-37b6e181a972ce6706fa61ad91a3f4994af90049.css",
         "stylesheets/github2-8159a6c9a2c0054ae5779d7c98fa8d0b5dd25fca.css") }
         { css(codemirror_css : _*) }
